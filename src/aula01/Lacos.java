dfacd162f3396b47eb7384cdb2b456baeef594f9
jessicavitoriatavares <jessica.vitoria.tavares044@gmail.com>
2023-06-07 18:25:29 -0300

    segundo commit

---
+++ src/aula01/Lacos.java
@@ -0,0 +1,17 @@
+package aula01;
+
+public class Lacos {
+
+	public static void main(String[] args) {
+	
+		int i = 0;
+		int soma = 0;
+		
+		while (i <= 100) {
+			soma = soma + i;
+			i++;
+		}
+		System.out.println("Soma: " + soma);
+	}
+
+}
